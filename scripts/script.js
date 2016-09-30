f0d9c07412f2ef2e7312b0998aaec439107cd83f
romandob <roman.dobrynin@gmail.com>
2016-09-30 11:35:10 +0300

    Updated feedback responsive block

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,19 @@
 
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
 
-    $('.navigate').on('click',function(e){
-        e.preventDefault();
-        $target = $($(this).attr('href'));
-        scrollTo($target);
-    });
+    'use strict';
+
+    // global variables
+    var $rootObjest = $('html'),
+        windowObject = $(window),
+        $document = $(document),
+        $alert = $('.alert'),
+        desktopMinSize = 721,
+        mobileSize = 720;
 
     $('.waypoint-nav').waypoint(function(){
         $('#primary-navigation').toggleClass('fixed');
     },{offset:'86px'});
-
-    // blog page
-
-    $('#load-more').on('click',function(e){
-
-        e.preventDefault();
-
-        var $content = $('#content');
-        var $this = $(this).attr('disabled',true);
-        var next = $(this).data('next');
-        var max = $(this).data('max');
-        var url = $(this).attr('href');
-
-        $.get(url, {paged: next}, function(data, textStatus, xhr) {
-
-            var html = $(data).find('#content').html();
-
-            $content.append(html);
-
-            $this.data('next',next++).removeAttr('disabled');
-
-            if (next > max) {
-                $this.remove();
-            }
-
-        });
-
-    });
 
     $(".mouse").click(function() {
         $('html, body').animate({
@@ -45,46 +21,8 @@ jQuery(document).ready(function($) {
         }, 400);
     });
 
-
-
-    function inputCheck($input){
-
-        if ($input.val() == '') {
-            $input.parent().addClass('error');
-        }else{
-            $input.parent().removeClass('error');
-        }
-
-    }
-
-
-    // work page
-
-    $('.project-excerpt').each(function(index) {
-        var ind = index * 175;
-        var test = $(this);
-        setTimeout(function(){
-            test.removeClass('hidden');
-        }, ind);
-    });
-
-    if (!Modernizr.touch) {
-
-        $(window).load(function(){
-
-            var workleftwidth = parseInt($('#work-pagers .left').outerWidth(),10);
-            var workrightwidth = parseInt($('#work-pagers .right').outerWidth(),10);
-
-            var left = workleftwidth-70;
-            var right = workrightwidth-70;
-
-            $('#work-pagers .left').css({marginLeft: '-'+ left +'px'}).removeClass('hidden');
-            $('#work-pagers .right').css({marginRight: '-'+ right + 'px'}).removeClass('hidden');
-
-
-        });
-
-    }
+    windowObject.load(mobileViewUpdate);
+    windowObject.resize(mobileViewUpdate);
 
     // mobile
 
@@ -99,42 +37,6 @@ jQuery(document).ready(function($) {
 
     });
 
-    preload();
-
-    $('.rollover').on('mouseenter',function(){
-        switchSrc($(this));
-    })
-
-    $('.rollover').on('mouseleave',function(){
-        switchSrc($(this));
-    })
-
-
-    // helpers
-
-    function scrollTo($element){
-        $('html, body').animate({scrollTop: $element.offset().top-90}, 500);
-    }
-
-    function switchSrc($object){
-
-        var original = $object.attr('src');
-        var replacement = $object.data('rollover');
-
-        $object.attr('src',replacement).data('rollover',original);
-
-    }
-
-    function preload(){
-
-        var images = [];
-
-        $('.rollover').each(function(i) {
-            images[i] = new Image();
-            images[i].src = $(this).data('rollover');
-        });
-
-    }
 
     $("#feedback-form").submit(function(e) {
 
@@ -149,19 +51,19 @@ jQuery(document).ready(function($) {
             {
                 console.log(data); // show response from the php script.
                 if(data.error == 0) {
-                    $('.alert').html('<p>Thank you for message!</p>').fadeIn('fast');
+                    $alert.html('<p>Thank you for message!</p>').fadeIn('fast');
                     setTimeout(function(){
                         $('#feedback').animate({bottom:'-564px'}, 200)
                     }, 200);
                     setTimeout(function(){
-                        $('.alert').fadeOut('fast');
+                        $alert.fadeOut('fast');
                     }, 2100);
-                    $('#mail-address, #mail-name, #mail-text').text();
+                    $('form')[0].reset();
                 }
                 else {
-                    $('.alert').html('<p>'+data.error+'</p>').fadeIn('fast');
+                    $alert.html('<p>'+data.error+'</p>').fadeIn('fast');
                     setTimeout(function(){
-                        $('.alert').fadeOut('fast');
+                        $alert.fadeOut('fast');
                     }, 2100);
                 }
             },
@@ -177,10 +79,32 @@ jQuery(document).ready(function($) {
 });
 
 function getInTouch() {
-    $('#feedback').animate({bottom:'1px'}, 200)
+    if($('html').hasClass('desktop')) {
+        $('#feedback').animate({bottom:'1px'}, 200)
+    }
+    else {
+        $('#feedback').animate({left:'0'}, 200)
+    }
+
 }
 
 function closeFeedback() {
-    $('#feedback').animate({bottom:'-564px'}, 200)
+    if($('html').hasClass('desktop')) {
+        $('#feedback').animate({bottom: '-564px'}, 200)
+    }
+    else {
+        $('#feedback').animate({left:'-100%'}, 200)
+    }
+}
+
+function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth <= 720) {
+        $("html").removeClass("desktop").addClass("mobile");
+    }
+    else {
+        $("html").removeClass("mobile").addClass("desktop");
+        $("#feedback").removeAttr('style');
+    }
 }
 
